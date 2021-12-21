7c4e8f6818d31f5ad19f412bdf013e83dbdc8b0a
FARJADU <54778361+FARJADU@users.noreply.github.com>
2021-12-21 14:36:33 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  us.hushmine.pro --user 0xA7C4c6fE4B7928C92E7743C634166966142C44eE.createthis/farjadu@gmail.com --pec --fee 0 --pass z --port 9009
